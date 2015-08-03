d4ac6c11216c8d558c88654506e560680041db16
Jerry Pallath <jpallath@nyu.edu>
2015-08-03 12:54:07 -0400

    fixed some controllers but still need to fix comments and password digest

---
+++ models/idea.js
@@ -0,0 +1,15 @@
+var mongoose = require('mongoose'),
+    Schema   = mongoose.Schema;
+
+var ideaSchema = Schema({
+    title: {type: String, required: true},
+    author: String,
+    content: String,
+    category: String,
+    image: String,
+    edited: {type: Date, default: Date.now}
+})
+
+var Idea = mongoose.model("Idea", ideaSchema);
+
+module.exports = Idea;
